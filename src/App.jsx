a6471d45e32a810d3f018a51ddeccd0261cdfbae
mmehtapavci <mmmehtapavci@gmail.com>
2024-10-26 18:41:52 +0300

    componentler açılıyor.Gerekli importlar yapılıyor.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import PageContainer from './container/PageContainer'
+import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  
+      <PageContainer>
+        <Header />
+      </PageContainer>  
     </>
   )
 }
