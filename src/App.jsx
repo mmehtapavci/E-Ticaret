88aea2a16ec8a699e0524ce2669a77fee883502e
mmehtapavci <mmmehtapavci@gmail.com>
2024-10-26 22:19:27 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import './App.css'
 import PageContainer from './container/PageContainer'
 import Header from './components/Header'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <PageContainer>
         <Header />
-      </PageContainer>  
-    </>
+      </PageContainer>
+    </div>  
   )
 }
 
