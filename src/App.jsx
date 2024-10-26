2a55abc30ee6101136bdb91009ed87e7966df14e
mmehtapavci <mmmehtapavci@gmail.com>
2024-10-26 23:14:34 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ function App() {
 
   return (
     <div>
-      <PageContainer>
-        <Header />
-      </PageContainer>
+     e ticaret
     </div>  
   )
 }
---
+++ src/container/PageContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageContainer() {
+  return (
+    <div>PageContainer</div>
+  )
+}
+
+export default PageContainer
