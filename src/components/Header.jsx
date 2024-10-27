dcb7176ff0d96341604b90ddd598c063c26db349
mmehtapavci <mmmehtapavci@gmail.com>
2024-10-28 00:03:36 +0300

    icon have been added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import '../css/Header.css';
+import { CiShoppingBasket } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
 
 function Header() {
   return (
+  <div className='logo-bar'>  
+    <div className='flex-row' >
+        <img className='logo' src="./src/images/logo.png" alt="" />
+        <p className='logo-text'>Lotus Ticaret</p>
+    </div>
+
     <div>
-        <img src="" alt="" />
+      <input className='search-input' type="text" placeholder='Bir Şeyler Ara...'/>
+      <div>
+      <CiLight />
+      <FaMoon />
+      <CiShoppingBasket />
+      </div>
+      
+    </div>
+
     </div>
   )
 }
