51e72e6012c7444594cb83e5ce5ee0b05530c47b
mmehtapavci <mmmehtapavci@gmail.com>
2024-11-01 19:02:42 +0300

    Dark theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/Header.css';
 import { CiShoppingBasket } from "react-icons/ci";
 import { CiLight } from "react-icons/ci";
 import { FaMoon } from "react-icons/fa";
 
 function Header() {
+    const [ theme, setTheme ] = useState(false);
+
+    const changeTheme = () => {
+        const root = document.getElementById("root");
+        if (theme){
+          root.style.backgroundColor = "black";
+          root.style.color ="#fff";
+        }
+        else {
+          root.style.backgroundColor = "#fff";
+          root.style.color = "black";
+        }
+        setTheme(!theme);
+    }
+
   return (
   <div className='logo-bar'>  
     <div className='flex-row' >
@@ -12,12 +27,12 @@ function Header() {
         <p className='logo-text'>Lotus Ticaret</p>
     </div>
 
-    <div>
+    <div className='flex-row' >
       <input className='search-input' type="text" placeholder='Bir Şeyler Ara...'/>
       <div>
-      <CiLight />
-      <FaMoon />
-      <CiShoppingBasket />
+        {theme ? <FaMoon className='icon' onClick={changeTheme} /> : <CiLight className='icon' onClick={changeTheme} />
+        }
+      <CiShoppingBasket className='icon' />
       </div>
       
     </div>
