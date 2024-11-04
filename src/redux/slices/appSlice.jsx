1897044eb680ab1a3c68800d1b931faf54deb384
mmehtapavci <mmmehtapavci@gmail.com>
2024-11-04 17:22:40 +0300

    store

---
+++ src/redux/slices/appSlice.jsx
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+const initialState = {
+    loading : false
+}
+
+export const appSlice = createSlice({
+    name : "app",
+    initialState,
+    reducers:{
+
+    },
+    extraReducers:(builder)=>{
+
+    }
+})
+
+export const {  } = appSlice.actions
+
+export default appSlice.reducer
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import appReducer from '../redux/slices/appSlice';
+import productReducer from '../redux/slices/productSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    app: appReducer,
+    product: productReducer
+  },
 })
