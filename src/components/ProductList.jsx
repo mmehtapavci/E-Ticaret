dfebb5a68ca6c8b27915b6e05b4b9d9688a91685
mmehtapavci <mmmehtapavci@gmail.com>
2024-11-11 21:33:51 +0300

    edit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react'
+import { useEffect } from 'react';
 import {useDispatch , useSelector }from 'react-redux'
 import { getAllProducts } from '../redux/slices/productSlice';
 import Product from './Product';
 
 function ProductList() {
 
-  const dispatch = useDispatch();
-  const {products} = useSelector((store)=> store.product)
+    const dispatch = useDispatch();
+    const { products } = useSelector((store) => store.product);
+    console.log(products)
 
   useEffect(() => {
-    dispatch(getAllProducts)
-  }, []);
-
+    dispatch(getAllProducts())
+  }, [])
   return (
-    <div>
-      {
-        products && products.map((product) => (
-            <Product key={product.id} product={product} />
-        ))
-      }
-    </div>
+      <div>
+        {
+          products && products.map((product) => (
+              <Product key={product.id} product={product} />
+          ))
+        }
+      </div>
   )
 }
 
