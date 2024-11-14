b72d2b3d9be64ebe8efe463b5e531cad2bfa36c2
mmehtapavci <mmmehtapavci@gmail.com>
2024-11-14 23:27:12 +0300

    edit code

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,11 +4,11 @@ import '../css/Product.css';
 
 function Product({ product }) {
   const{ id , price , image , title , description} = product;
-   return (
+    return (
     <div  className='card'>
         <img className='image' src={image} alt="" />
         <div>
-         <p className='title' >{title}</p>
+          <p className='title' >{title}</p>
           <h3 className='price' >{price} ₺</h3>
         </div>
         <div className='flex-row' >
